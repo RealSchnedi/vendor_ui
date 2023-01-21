539d91f358b45dbe26861308240f542407e71a4e
RealSchnedi <schnedi3@gmail.com>
2023-01-21 21:37:34 +0100

    Import default wallpaper

---
+++ ui.mk
@@ -0,0 +1,8 @@
+#
+#   Copyright (C) 2023
+#   SPDX-License-Identifier: Apache-2.0
+#
+
+# Overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/ui/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/ui/overlay
